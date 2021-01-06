78d555d8878c58a31b285bbd5dcc38ca9c785a82
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2021-01-06 12:24:23 +0100

    Add files via upload

--- ml/model.py
+++ ml/model.py
@@ -4,4 +4,13 @@ def DataManager(d:DataHandler=None, fr: FeatureRecipe=None, fe:FeatureExtractor=
     """
         Fonction qui lie les 3 premières classes de la pipeline et qui return FeatureExtractor.split(0.1)
     """
-    pass
+    dt = DataHandler()
+    dt.get_process_data()
+    fr = FeatureRecipe(dt.df_res)
+    fr.get_process_data(0.3)
+
+    flist = ['listing_id','beds','pricing_weekly_factor','pricing_monthly_factor','name','type','city','neighborhood','latitude','longitude','is_rebookable','is_new_listing','is_fully_refundable','is_host_highly_rated']
+    fe = FeatureExtractor(dt.df_res,flist)
+    X_train,X_test,y_train,y_test = fe.get_process_data()
+
+    return X_train,X_test,y_train,y_test
